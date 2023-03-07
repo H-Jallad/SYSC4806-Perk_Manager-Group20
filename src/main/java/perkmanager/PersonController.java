fa9203eb059f0d3396904fb037d80fde4f0791d6
areebhaq14 <114181664+areebhaq14@users.noreply.github.com>
2023-03-07 18:15:42 -0500

    Membership Controller methods completed and added 3 thymeleaf views

--- src/main/java/perkmanager/PersonController.java
+++ src/main/java/perkmanager/PersonController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Member;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -37,12 +38,32 @@ public class PersonController {
         return ResponseEntity.ok(membership1);
     }
 
-//    @PostMapping("/createUser")
-//    public ResponseEntity<Person> createUser() {
-//        Person newPerson = new Person();
-//        personRepository.save(newPerson);
-//        return new ResponseEntity<>(newPerson, HttpStatus.OK);
-//    }
+    @GetMapping("/membership")
+    public String showMembershipForm(Model model) {
+    model.addAttribute("membershipTypes", Arrays.asList("CAA", "AMEX"));
+    model.addAttribute("membership", new Membership());
+    return "membership";
+    }
+
+    @PostMapping("/membership")
+    public String membershipSubmit(@ModelAttribute Membership membership, Model model) {
+        membershipRepository.save(membership);
+        Person person = personRepository.findById(1);
+        person.addMembership(membership);
+        personRepository.save(person);
+        model.addAttribute("membership", membership);
+        return "display-membership";
+    }
+
+    @GetMapping("/memberships-view")
+    public String membershipView(Model model){
+        Person person = personRepository.findById(1);
+        List<Membership> memberships = person.getMembershipList();
+        model.addAttribute("memberships", memberships);
+
+        return "personsMemberships";
+    }
+
 
     @GetMapping("/perk-manager")
     public String addUser(Model model)
@@ -56,9 +77,9 @@ public class PersonController {
     @PostMapping("/perk-manager/view")
     public String userSubmit(@ModelAttribute Person newPerson, Model model)
     {
-
         personRepository.save(newPerson);
         model.addAttribute("newPerson", newPerson);
         return "result";
     }
+
 }
