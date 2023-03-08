f82ad18f56aab81baca2af7d4ffd011087de45b4
areebhaq14 <114181664+areebhaq14@users.noreply.github.com>
2023-03-07 20:48:39 -0500

    Updated PersonController with perk post and get methods and added new views for perk

--- src/main/java/perkmanager/PersonController.java
+++ src/main/java/perkmanager/PersonController.java
@@ -1,17 +1,14 @@
 package perkmanager;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.lang.reflect.Member;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 public class PersonController {
@@ -28,14 +25,27 @@ public class PersonController {
         Iterable<Person> people = personRepository.findAll();
         return ResponseEntity.ok(people);
     }
-    @RequestMapping("/addPerk")
-    public ResponseEntity<Membership> addPerk(@RequestParam(value="membership") int membership, @RequestParam(value = "name") String name, @RequestParam(value = "description") String description){
-        Membership membership1 = new Membership();
-        membership1.setName("AMEX");
-        membershipRepository.save(membership1);
-        membershipService.updateMembership(membership1.getName(), name, description);
 
-        return ResponseEntity.ok(membership1);
+    @GetMapping("/addPerk")
+    public String showPerkForm(Model model){
+        model.addAttribute("membershipType",Arrays.asList("CAA", "AMEX"));
+        model.addAttribute("perk", new Perk());
+        return "perk";
+    }
+    @PostMapping("/addPerk")
+    public String addPerk(@ModelAttribute Membership membership, @ModelAttribute Perk perk, Model model) {
+        Membership existingMembership = membershipRepository.findByName(membership.getName());
+        existingMembership.addPerk(perk);
+        membershipRepository.save(existingMembership);
+        model.addAttribute("perk", perk);
+        model.addAttribute(("membership"),existingMembership);
+        return "listOfPerks";
+    }
+    @GetMapping("/perks-view")
+    public String perksView(Model model){
+        List<Membership> memberships = membershipRepository.findAll();
+        model.addAttribute("memberships", memberships);
+        return "allPerks";
     }
 
     @GetMapping("/membership")
